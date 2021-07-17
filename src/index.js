4712d6d4abe447a94804596b492bb24a90ad5b7b
Panchaya-M <panchaya.meeboonpor@gmail.com>
2021-07-17 11:02:30 +0700

    Update headers

--- src/index.js
+++ src/index.js
@@ -2,21 +2,23 @@ import './style.scss'
 
 document.addEventListener('DOMContentLoaded', function() {
     let listHeroesDOM = document.getElementById('list-heroes')
-
+    
     if(listHeroesDOM == null) { return }
 
     let url = process.env.API_URL + "/heroes"
     fetch(url, {
         method: "GET",
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': process.env.API_CREDENTIAL
         }
     })
     .then(resp => resp.json())
     .then(data => {
-        addHeaderTitleToHeroesList(listHeroesDom)
         buildHeroDom(listHeroesDOM, data)  
-    })
+        addHeaderTitleToHeroesList(listHeroesDom)
+        
+     })
 })
 
 function addHeaderTitleToHeroesList(targetDom) {
@@ -35,11 +37,13 @@ function buildHeroDom(targetDom, data) {
     data.forEach(hero => {
         let htmlStr = `
             <div class="hero">
-                <a href="">${hero.name}</a>
+                <a href="" class="hero-name">${hero.name}</a>
                 <div>${hero.level}</div>
                 <div>${hero.hp}</div>
                 <div>${hero.mp}</div>
                 <div>${hero.job}</div>
+            </div>
         `
+        targetDom.insertAdjacentHTML('beforeend', htmlStr)
     })
 }
